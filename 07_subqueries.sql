71e4e305b77d60e7a8505f0dc351e5aaa1003dba
srayanroy <srayanroy2@gmail.com>
2026-09-18 16:39:36 +0530

    Add files via upload

---
+++ 07_subqueries.sql
@@ -0,0 +1,60 @@
+use marketplace_db;
+select *
+from orders 
+where total_amount > (select max(total_amount)
+from orders
+where payment_status = 'pending');
+select * 
+from orders 
+where total_amount < (select avg(total_amount)
+from orders 
+where payment_status = 'paid');
+select * 
+from orders 
+where total_amount = (select max(total_amount)
+from orders );
+select * 
+from orders 
+where total_amount = (select max(total_amount)
+from orders );
+select *
+from orders
+where total_amount > (select avg(total_amount)
+from orders );
+select * from
+orders 
+where total_amount < (select min(total_amount)
+from orders 
+where payment_status = 'paid' );
+select 
+* from 
+orders 
+where total_amount > (select avg(total_amount)
+from orders 
+where payment_status = 'paid') ;
+select * 
+from orders 
+where total_amount = (select min(total_amount)
+from orders 
+);
+SELECT *
+FROM customers
+WHERE customer_id IN (
+    SELECT customer_id
+    FROM orders
+);
+select * from customers 
+where customer_id in (select customer_id 
+from orders );
+select 
+* from customers 
+where customer_id not in ( select (customer_id)
+from 
+orders);
+select * from products 
+where proudct_id in (select seller_product_id 
+from seller_products where (select order_items_id 
+from order_items);
+select * from products where 
+product_id in (select  seller_product_id
+from seller_products where seller_product_id in (select order_item_id from order_items ));
